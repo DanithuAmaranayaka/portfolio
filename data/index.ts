cbf76cda7dc56ad08efffa278f80e39df67c6b38
Danithu <danithuamaranayakanew@gmail.com>
2024-05-20 00:03:56 +0530

    hi

---
+++ data/index.ts
@@ -0,0 +1,219 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "AA Autocare(PVT)LTD.",
+    des: "An innovative e-commerce web application for a tire shop, facilitating seamless browsing, purchase, and delivery of a wide range of tires for various vehicles, complemented by personalized recommendations..",
+    img: "/aaautocare.png",
+    iconLists: ["/html.webp", "/css.webp", "/js.png", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Fivem Arena Sever",
+    des: "The FiveM Arena Server offers an unparalleled competitive environment for Grand Theft Auto V (GTA V) players. This specialized server is designed for high-intensity PvP (player versus player) activities, featuring various game modes, custom maps, and balanced gameplay to ensure a thrilling experience for all participants.",
+    img: "/fivemarina.jpg",
+    iconLists: ["/lua.png", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/cfx.re/join/36meob",
+  },
+  {
+    id: 3,
+    title: "AI Image SaaS - Canva Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "I thought it was impossible to make a website as beautiful as our product, but Danithu proved me wrong..",
+    name: "Chirath Timodh",
+    title: "Director of Technovizion",
+  },
+  {
+    quote:
+      "The expertise and abilities of this Fiverr were necessary for the creation of our blog and website. More importantly for me, he took the time to immerse himself in our vision, mission, core values, and commitments. His time, communication, collaboration, problem-solving, and time management were excellent. His kindness and patience made it a joy to work with him. Finally, he has continued to make modifications at no additional charge.",
+    name: "larrymiller3266",
+    title: "United States",
+  },
+  {
+    quote:
+      "One of the best freelancer I ever had! And I had a lot before! All questions are answerd in time, and changes are made better than I asked. Great guy. I only can recommend such a word-press-profi.",
+    name: "concraduate",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Swapon Mia delivered EXCEPTIONALLY on our website development project, with an unparalleled attention to detail that truly sets him apart. Working with him was a breeze due to his fantastic level of cooperation. Highly recommend! 👍",
+    name: "jon111092",
+    title: "United States",
+  },
+  {
+    quote:
+      "Amazing!!!! HIGHLY recommend for anyone looking to design and build a website. THe seller went above and beyond to deliver the final result ahead of schedule and better than I even expected. He only designs all of our websites and have a few more in the pipeline with him. Thank you so much!!!!",
+    name: "rishipopat",
+    title: "United States",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - JSM Tech",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    
+    
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
